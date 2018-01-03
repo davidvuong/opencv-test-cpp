a78bf9bf1b32ac6ecd822f886a3f1d29dbbb070e
David Vuong <david.vuong256@gmail.com>
2018-01-03 17:02:11 +1100

    chore(cmake): add more vars to mingw-w64 toolchain

--- Toolchain-linux-mingw-w64.cmake
+++ Toolchain-linux-mingw-w64.cmake
@@ -1,8 +1,17 @@
 set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_SYSTEM_VERSION 10)
 
 set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
 set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
+SET(CMAKE_RC_COMPILER x86_64-w64-mingw32-windres)
 
 # https://stackoverflow.com/questions/19450722/include-mingw-libraries-in-compiled-file
 # https://arrayfire.com/cross-compile-to-windows-from-linux/
 set(CMAKE_EXE_LINKER_FLAGS "-static-libgcc -static-libstdc++ -static")
+
+# Don't assume libraries will exist on the host platform, bundle it into the binary.
+SET(BUILD_SHARED_LIBS OFF)
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
